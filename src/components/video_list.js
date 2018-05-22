51b7d29914f9f1497b310eeeae9823249372704d
sleeping-buddha <37425417+sleeping-buddha@users.noreply.github.com>
2018-05-22 01:35:42 -0400

    Initial commit

---
+++ src/components/video_list.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import VideoItemList from './video_list_item';
+
+const VideoList = (props) => {
+    const videoItems = props.videos.map((video) => {
+        return <VideoItemList
+            onVideoSelect={props.onVideoSelect}
+            key={video.etag}
+            video={video}/>
+    });
+    return (
+        <ul className="col-md-4 list-group">
+            {videoItems}
+        </ul>
+    );
+};
+
+export default VideoList;
---
+++ src/index.js
@@ -0,0 +1,43 @@
+import React, {Component} from 'react';
+import ReactDom from 'react-dom';
+import SearchBar from './components/search_bar';
+import YTSearch from 'youtube-api-search';
+import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
+
+const API_KEY = "AIzaSyCJBTlf2ZSQa3FJXIJ104TlUwTp3-RiUwM";
+
+
+
+class App extends Component {
+    constructor(props){
+        super(props);
+
+        this.state = { videos: [], selectedVideo: null};
+
+        this.videoSearch("surfboard");
+    }
+
+    videoSearch(term) {
+        YTSearch({key: API_KEY, term: term}, (videos) => {
+            this.setState(
+                {
+                    videos : videos,
+                    selectedVideo: videos[0]
+                });
+        });
+    }
+
+    render() {
+        return (
+            <div>
+                <SearchBar onSearchTermChange={term => this.videoSearch(term)}/>
+                <VideoDetail video={this.state.selectedVideo}/>
+                <VideoList onVideoSelect={selectedVideo => this.setState({selectedVideo})}
+                    videos={this.state.videos}/>
+            </div>
+        );
+    }
+};
+
+ReactDom.render(<App />, document.querySelector('.container'));
