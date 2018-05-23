e34375604ff7aea9637cb80995d6c79a11ec0955
sleeping-buddha <37425417+sleeping-buddha@users.noreply.github.com>
2018-05-22 22:30:15 -0400

    Throttle the search query

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import SearchBar from './components/search_bar';
 import YTSearch from 'youtube-api-search';
 import VideoList from './components/video_list';
 import VideoDetail from './components/video_detail';
+import _ from 'lodash'
 
 const API_KEY = "AIzaSyCJBTlf2ZSQa3FJXIJ104TlUwTp3-RiUwM";
 
@@ -29,9 +30,12 @@ class App extends Component {
     }
 
     render() {
+
+        const videoSearch =  _.debounce((term) => { this.videoSearch(term)}, 300);
+
         return (
             <div>
-                <SearchBar onSearchTermChange={term => this.videoSearch(term)}/>
+                <SearchBar onSearchTermChange={videoSearch}/>
                 <VideoDetail video={this.state.selectedVideo}/>
                 <VideoList onVideoSelect={selectedVideo => this.setState({selectedVideo})}
                     videos={this.state.videos}/>
